092c6b152920586214f0c9c839848cfcca51bd97
Khushal Mali <khushalmali8448@gmail.com>
2025-08-08 00:49:57 +0530

    initialized the game

---
+++ screens/StartGameScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const StartGameScreen = () => {
+  return (
+    <View>
+      <TextInput />
+      <Text>Buttons</Text>
+    </View>
+  );
+};
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({});
